ef4b1fd03aaea084dbe01048af20ab218cecf764
Michael Hendricks <michael@ndrix.org>
2013-12-06 16:26:04 -0700

    A lot of progress on quickcheck
    Both arbitrary/2 and shrink/3 have reasonably complete definitions.  The
    next step is to implement quickcheck/1 so that something like this
    works:
    
      prop_reverse_length(L:list) :-
          length(L, Len),
          reverse(L, RL),
          length(RL, Len).
    
      ?- quickcheck(prop_reverse_length).
      +++ OK, passed 100 tests.
      true.
    
    Then I can build TAP and PlUnit support on top of quickcheck/1 or maybe
    quickcheck/2 which accepts options.

--- prolog/quickcheck.pl
+++ prolog/quickcheck.pl
@@ -1,3 +1,132 @@
-:- module(quickcheck, []).
+:- module(quickcheck, [ arbitrary/2
+                      , arbitrary_type/1
+                      , shrink/3
+                      ]).
+:- use_module(library(apply), [maplist/2]).
+:- use_module(library(error), []).
+:- use_module(library(random), [random_between/3, random_member/2]).
 
-% TODO implement the module
+:- multifile error:has_type/2.
+error:has_type(arbitrary_type, Type) :-
+    nonvar(Type),
+    \+ \+ quickcheck:arbitrary_type(Type).
+
+%% arbitrary(+Type, -Value) is det.
+%
+%  Generate a random Value of Type. If you define your own types, add
+%  clauses to this multifile predicate to support them in quickcheck.
+:- multifile arbitrary/2.
+arbitrary(any, X) :-
+    setof( Type
+         , ( arbitrary_type(Type)
+           , ground(Type)  % exclude parameterized types
+           , Type \== any  % don't recurse
+           )
+         , Types
+         ),
+    random_member(Type, Types),
+    arbitrary(Type, X).
+arbitrary(atom, X) :-
+    arbitrary(codes, Codes),
+    atom_codes(X, Codes).
+arbitrary(between(L,U), X) :-
+    random_between(L,U,X).
+arbitrary(boolean, X) :-
+    random_member(X, [true, false]).
+arbitrary(chars, X) :-
+    arbitrary(atom, Atom),
+    atom_chars(Atom, X).
+arbitrary(code, X) :-
+    random_between(0x20, 0x7e, X).  % printable ASCII
+arbitrary(codes, X) :-
+    arbitrary(list(code), X).
+arbitrary(encoding, X) :-
+    setof(E, error:current_encoding(E), Encodings),
+    random_member(X, Encodings).
+arbitrary(float, X) :-
+    arbitrary(integer, I),
+    X is I * random_float.
+arbitrary(integer, X) :-
+    random_between(-30_000, 30_000, X).
+arbitrary(list, X) :-
+    arbitrary(list(any), X).
+arbitrary(list(T), X) :-
+    random_between(1,30,Length),
+    length(X, Length),
+    maplist(arbitrary(T), X).
+arbitrary(negative_integer, X) :-
+    random_between(-30_000, 1, X).
+arbitrary(nonneg, X) :-
+    random_between(0, 30_000, X).
+arbitrary(number, X) :-
+    random_member(Type, [integer, float]),
+    arbitrary(Type, X).
+arbitrary(oneof(L), X) :-
+    random_member(X, L).
+arbitrary(positive_integer, X) :-
+    random_between(1, 30_000, X).
+arbitrary(rational, X) :-
+    arbitrary(integer, Numerator),
+    arbitrary(integer, Denominator),
+    X is Numerator rdiv Denominator.
+arbitrary(string, X) :-
+    arbitrary(codes, Codes),
+    string_codes(X, Codes).
+arbitrary(text, X) :-
+    random_member(Type, [atom, string, chars, codes]),
+    arbitrary(Type, X).
+
+
+%% arbitrary_type(?Type) is multi
+%
+%  True if Type supports arbitrary/2.
+arbitrary_type(Type) :-
+    clause(arbitrary(Type, _), _).
+
+
+%% shrink(+Type, +Value, -Smaller) is nondet.
+%
+%  True if Smaller is a "smaller" version of Value according to the
+%  semantics of Type. This predicate is called after
+%  quickcheck finds a Value for which a property fails. By shrinking
+%  values, we can obtain a more minimal test case.
+%
+%  When defining shrink/3 for your own types, be sure to fail if Value
+%  cannot be shrunk any smaller. It's acceptable to produce additional
+%  shrunken values on backtracking.
+:- multifile shrink/3.
+shrink(_, X, Y) :- % integral types
+    integer(X),
+    Y is sign(X).
+shrink(atom, _, X) :-
+    shrink(codes, _, Codes),
+    atom_codes(X, Codes).
+shrink(code, _, 0'a).
+shrink(code, _, 0'b).
+shrink(code, _, 0'c).
+shrink(code, _, 0' ).
+shrink(codes, _, []).
+shrink(codes, _, [Code]) :-
+    shrink(code, _, Code).
+shrink(list, _, []).
+shrink(list, [_|T], T).
+shrink(list, [H|T0], [H|T]) :-
+    shrink(list, T0, T).
+shrink(list(_), _, []).
+shrink(list(_), [_|T], T).
+shrink(list(Type), [H0|T0], [H|T]) :-
+    ( shrink(Type, H0, H)
+    ; H = H0
+    ),
+    shrink(list(Type), T0, T).
+shrink(string, _, X) :-
+    shrink(codes, _, Codes),
+    string_codes(X, Codes).
+
+
+%% quickcheck(+Property:atom) is semidet.
+%
+%  True if Property holds many random values.
+:- meta_predicate quickcheck(+).
+quickcheck(Module:Property) :-
+    todo.
