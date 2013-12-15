df3745b55d43f3bd56b27101135fcfc4f0f55c19
Michael Hendricks <michael@ndrix.org>
2013-12-14 19:53:37 -0700

    Release v0.0.2

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(quickcheck).
 title('QuickCheck randomized testing').
 
-version('0.0.1').
-download('http://packs.ndrix.com/quickcheck/quickcheck-0.0.1.tgz').
+version('0.0.2').
+download('http://packs.ndrix.com/quickcheck/quickcheck-0.0.2.tgz').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
