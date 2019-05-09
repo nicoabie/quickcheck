7f28f253179d974335dd926c44b86983de1a38ae
Nicolás Andrés Gallinal <ngallinal@hexacta.com>
2019-05-09 16:33:27 -0300

    Release 0.2.3

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(quickcheck).
 title('QuickCheck randomized testing').
 
-version('0.2.2').
-download('https://github.com/nicoabie/quickcheck/archive/v0.2.2.zip').
+version('0.2.3').
+download('https://github.com/nicoabie/quickcheck/archive/v0.2.3.zip').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Nico Gallinal','nicoabie@gmail.com').
